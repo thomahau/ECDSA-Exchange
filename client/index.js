ea99ef6826bf5650aff48d151c05a31bc0ba43e7
Thomas Hauge <thomas.sommerfelt.hauge@gmail.com>
2022-01-22 10:24:12 +0100

    sets up public key balances and digital signatures for private key verification

--- client/index.js
+++ client/index.js
@@ -1,34 +1,46 @@
-import "./index.scss";
+import './index.scss';
 
-const server = "http://localhost:3042";
+const server = 'http://localhost:3042';
 
-document.getElementById("exchange-address").addEventListener('input', ({ target: {value} }) => {
-  if(value === "") {
-    document.getElementById("balance").innerHTML = 0;
+document.getElementById('exchange-address').addEventListener('input', ({ target: { value } }) => {
+  if (value === '') {
+    document.getElementById('balance').innerHTML = 0;
     return;
   }
 
-  fetch(`${server}/balance/${value}`).then((response) => {
-    return response.json();
-  }).then(({ balance }) => {
-    document.getElementById("balance").innerHTML = balance;
-  });
+  fetch(`${server}/balance/${value}`)
+    .then(response => {
+      return response.json();
+    })
+    .then(({ balance }) => {
+      document.getElementById('balance').innerHTML = balance;
+    });
 });
 
-document.getElementById("transfer-amount").addEventListener('click', () => {
-  const sender = document.getElementById("exchange-address").value;
-  const amount = document.getElementById("send-amount").value;
-  const recipient = document.getElementById("recipient").value;
+document.getElementById('transfer-amount').addEventListener('click', async () => {
+  const sender = document.getElementById('exchange-address').value;
+  const amount = document.getElementById('send-amount').value;
+  const recipient = document.getElementById('recipient').value;
+  const privateKey = document.getElementById('private-key').value;
 
   const body = JSON.stringify({
-    sender, amount, recipient
+    sender,
+    amount,
+    recipient,
+    privateKey,
   });
 
   const request = new Request(`${server}/send`, { method: 'POST', body });
 
-  fetch(request, { headers: { 'Content-Type': 'application/json' }}).then(response => {
-    return response.json();
-  }).then(({ balance }) => {
-    document.getElementById("balance").innerHTML = balance;
-  });
+  fetch(request, { headers: { 'Content-Type': 'application/json' } })
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+      if (data.balance) {
+        document.getElementById('balance').innerHTML = data.balance;
+      } else {
+        document.getElementById('error').innerHTML = data.error;
+      }
+    });
 });
--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
+const { getPublicKey, sign, verify, utils } = require('ethereum-cryptography/secp256k1');
 const express = require('express');
 const app = express();
 const cors = require('cors');
+
 const port = 3042;
 
 // localhost can have cross origin errors
@@ -8,25 +10,61 @@ const port = 3042;
 app.use(cors());
 app.use(express.json());
 
-const balances = {
-  "1": 100,
-  "2": 50,
-  "3": 75,
+const keypairs = {};
+const balances = {};
+
+for (let i = 0; i < 3; i++) {
+  const privateKeyArray = utils.randomPrivateKey();
+  const publicKeyArray = getPublicKey(privateKeyArray);
+  const address = '0x' + utils.bytesToHex(publicKeyArray).slice(-40);
+
+  keypairs[address] = { publicKey: utils.bytesToHex(publicKeyArray), privateKey: utils.bytesToHex(privateKeyArray) };
+  balances[address] = Math.floor(Math.random() * (100 - 1) + 1);
 }
 
 app.get('/balance/:address', (req, res) => {
-  const {address} = req.params;
+  const { address } = req.params;
   const balance = balances[address] || 0;
   res.send({ balance });
 });
 
-app.post('/send', (req, res) => {
-  const {sender, recipient, amount} = req.body;
-  balances[sender] -= amount;
-  balances[recipient] = (balances[recipient] || 0) + +amount;
-  res.send({ balance: balances[sender] });
+app.post('/send', async (req, res) => {
+  const { sender, recipient, amount, privateKey } = req.body;
+
+  try {
+    const messageHash = await utils.sha256(JSON.stringify({ sender, amount, recipient }));
+    const signature = await sign(messageHash, privateKey);
+    const isSigned = verify(signature, messageHash, keypairs[sender].publicKey);
+
+    if (isSigned) {
+      balances[sender] -= amount;
+      balances[recipient] = (balances[recipient] || 0) + +amount;
+      res.send({ balance: balances[sender] });
+    } else {
+      res.status(403).send({ error: 'Invalid private key' });
+    }
+  } catch (error) {
+    res.status(403).send({ error: 'Invalid private key' });
+  }
 });
 
 app.listen(port, () => {
+  const addresses = Object.keys(balances);
+  const addressBalances = Object.values(balances);
+
+  console.log(`
+  Available Accounts
+  ==================
+  (0) ${addresses[0]} (Balance: ${addressBalances[0]})
+  (1) ${addresses[1]} (Balance: ${addressBalances[1]})
+  (2) ${addresses[2]} (Balance: ${addressBalances[2]})
+
+  Private Keys
+  ==================
+  (0) ${keypairs[addresses[0]].privateKey}
+  (1) ${keypairs[addresses[1]].privateKey}
+  (2) ${keypairs[addresses[2]].privateKey}
+  `);
+
   console.log(`Listening on port ${port}!`);
 });
